54c0c1353a8bb5fd73788af4c8a78cf677dbf097
Karen <wijaya.imd@gmail.com>
2017-03-18 13:17:45 +0800

    V 0.7

--- app/Http/Controllers/backend/DashboardController.php
+++ app/Http/Controllers/backend/DashboardController.php
@@ -11,6 +11,17 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('backend/dashboard/index');
+        //dd(\DB::table('transaksi')->select(\DB::Raw('SUM(total+denda) as profit'))->whereRaw("MONTH(created_at) = ".date('m'))->get());
+        $chart = '[';
+        for($i=1; $i<=12; $i++){
+            $_profit = \DB::table('transaksi')->select(\DB::Raw('SUM(total+denda) as profit'))->whereRaw("MONTH(created_at) = ".$i)->get()[0]->profit;
+            $profit = $_profit == '' ? 0 : $_profit;
+            $chart.='['.'"'.strtoupper(date('M', strtotime('01-' . $i . '-2017'))).'",'.$profit.'],';
+        }
+        $chart = substr($chart, 0, -1).']';
+        //dd($chart);
+        return view('backend/dashboard/index',[
+            'chart'=>$chart
+        ]);
     }
 }
--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -11,8 +11,8 @@
     <div class="page-head">
         <!-- BEGIN PAGE TITLE -->
         <div class="page-title">
-            <h1>Blank Page Layout
-                <small>blank page layout</small>
+            <h1>Dashboard
+                <small>statistics, charts, recent events and reports</small>
             </h1>
         </div>
         <!-- END PAGE TITLE -->
@@ -21,27 +21,112 @@
     <!-- BEGIN PAGE BREADCRUMB -->
     <ul class="page-breadcrumb breadcrumb">
         <li>
-            <a href="index-2.html">Home</a>
+            <a href="{{ url('backend') }}">Home</a>
             <i class="fa fa-circle"></i>
         </li>
         <li>
-            <a href="#">Blank Page</a>
-            <i class="fa fa-circle"></i>
-        </li>
-        <li>
-            <span class="active">Page Layouts</span>
+            <span class="active">Dashboard</span>
         </li>
     </ul>
     <!-- END PAGE BREADCRUMB -->
     <!-- BEGIN PAGE BASE CONTENT -->
-    <div class="note note-info">
-        <p> A black page template with a minimal dependency assets to use as a base for any custom page you create </p>
+    <div class="row widget-row">
+        <div class="col-md-4">
+            <!-- BEGIN WIDGET THUMB -->
+            <div class="widget-thumb widget-bg-color-white text-uppercase margin-bottom-20 bordered">
+                <h4 class="widget-thumb-heading">Monthly Transaction</h4>
+                <div class="widget-thumb-wrap">
+                    <i class="widget-thumb-icon bg-green icon-bulb"></i>
+                    <div class="widget-thumb-body">
+                        <span class="widget-thumb-subtitle">{{ date('F') }}</span>
+                        <span class="widget-thumb-body-stat" data-counter="counterup" data-value="{{ \App\Models\Transaksi::whereRaw("MONTH(created_at) = ".date('m'))->count() }}">0</span>
+                    </div>
+                </div>
+            </div>
+            <!-- END WIDGET THUMB -->
+        </div>
+        <div class="col-md-4">
+            <!-- BEGIN WIDGET THUMB -->
+            <div class="widget-thumb widget-bg-color-white text-uppercase margin-bottom-20 bordered">
+                <h4 class="widget-thumb-heading">Monthly Sales</h4>
+                <div class="widget-thumb-wrap">
+                    <i class="widget-thumb-icon bg-red icon-layers"></i>
+                    <div class="widget-thumb-body">
+                        <span class="widget-thumb-subtitle">IDR</span>
+                        <span class="widget-thumb-body-stat" data-counter="counterup" data-value="{{ \DB::table('transaksi')->select(\DB::Raw('SUM(total+denda) as profit'))->whereRaw("MONTH(created_at) = ".date('m'))->get()[0]->profit }}">0</span>
+                    </div>
+                </div>
+            </div>
+            <!-- END WIDGET THUMB -->
+        </div>
+        <div class="col-md-4">
+            <!-- BEGIN WIDGET THUMB -->
+            <div class="widget-thumb widget-bg-color-white text-uppercase margin-bottom-20 bordered">
+                <h4 class="widget-thumb-heading">Kendaraan</h4>
+                <div class="widget-thumb-wrap">
+                    <i class="widget-thumb-icon bg-purple icon-screen-desktop"></i>
+                    <div class="widget-thumb-body">
+                        <span class="widget-thumb-subtitle">Unit</span>
+                        <span class="widget-thumb-body-stat" data-counter="counterup" data-value="{{ \App\Models\Kendaraan::count() }}">0</span>
+                    </div>
+                </div>
+            </div>
+            <!-- END WIDGET THUMB -->
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12 col-xs-12 col-sm-12">
+            <!-- BEGIN PORTLET-->
+            <div class="portlet light bordered">
+                <div class="portlet-title">
+                    <div class="caption">
+                        <i class="icon-bar-chart font-dark hide"></i>
+                        <span class="caption-subject font-dark bold uppercase">Site Visits</span>
+                        <span class="caption-helper">weekly stats...</span>
+                    </div>
+                </div>
+                <div class="portlet-body">
+                    <div id="site_statistics_loading">
+                        <img src="{{ url('assets') }}/backend/global/img/loading.gif" alt="loading" /> </div>
+                    <div id="site_statistics_content" class="display-none">
+                        <div id="site_statistics" class="chart"> </div>
+                    </div>
+                </div>
+            </div>
+            <!-- END PORTLET-->
+        </div>
     </div>
     <!-- END PAGE BASE CONTENT -->
 @endsection
 
 @push('plugin_scripts')
+<script src="{{ url('assets') }}/backend/global/plugins/counterup/jquery.waypoints.min.js" type="text/javascript"></script>
+<script src="{{ url('assets') }}/backend/global/plugins/counterup/jquery.counterup.min.js" type="text/javascript"></script>
+<script src="{{ url('assets') }}/backend/global/plugins/flot/jquery.flot.min.js" type="text/javascript"></script>
+<script src="{{ url('assets') }}/backend/global/plugins/flot/jquery.flot.resize.min.js" type="text/javascript"></script>
+<script src="{{ url('assets') }}/backend/global/plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>
 @endpush
 
 @push('scripts')
+<script>
+    var Dashboard=function(){
+        return{
+            initCharts:function(){
+                function e(e,t,a,i){
+                    $('<div id="tooltip" class="chart-tooltip">'+i+"</div>").css({position:"absolute",display:"none",top:t-40,left:e-40,border:"0px solid #ccc",padding:"2px 6px","background-color":"#fff"}).appendTo("body").fadeIn(200)
+                }
+                if(jQuery.plot){
+                    var t=<?= $chart ?>;
+
+                    if(0!=$("#site_statistics").size()){
+                        $("#site_statistics_loading").hide(), $("#site_statistics_content").show();
+                        var a=($.plot($("#site_statistics"),[{data:t,lines:{fill:.6,lineWidth:0},color:["#f89f9f"]},{data:t,points:{show:!0,fill:!0,radius:5,fillColor:"#f89f9f",lineWidth:3},color:"#fff",shadowSize:0}],{xaxis:{tickLength:0,tickDecimals:0,mode:"categories",min:0,font:{lineHeight:14,style:"normal",variant:"small-caps",color:"#6F7B8A"}},yaxis:{ticks:5,tickDecimals:0,tickColor:"#eee",font:{lineHeight:14,style:"normal",variant:"small-caps",color:"#6F7B8A"}},grid:{hoverable:!0,clickable:!0,tickColor:"#eee",borderColor:"#eee",borderWidth:1}}),null);$("#site_statistics").bind("plothover",function(t,i,l){if($("#x").text(i.x.toFixed(2)),$("#y").text(i.y.toFixed(2)),l){if(a!=l.dataIndex){a=l.dataIndex,$("#tooltip").remove();l.datapoint[0].toFixed(2),l.datapoint[1].toFixed(2);e(l.pageX,l.pageY,l.datapoint[0],"Rp. "+l.datapoint[1])}}else $("#tooltip").remove(),a=null})}
+                }
+            },
+            init:function(){this.initCharts()}
+        }
+    }();
+    App.isAngularJsApp()===!1&&jQuery(document).ready(function(){Dashboard.init()});
+</script>
 @endpush
